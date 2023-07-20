7289611e1712a5c6f34ad6fb21c8a30bc0b16028
itc-s22029 <s22029@std.it-college.ac.jp>
2023-07-20 14:39:56 +0900

    chap5.1.2 commit

---
+++ ListViewSample/app/src/main/java/jp/ac/it_college/std/s22029/listviewsample/MainActivity.kt
@@ -0,0 +1,29 @@
+package jp.ac.it_college.std.s22029.listviewsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
+import jp.ac.it_college.std.s22029.listviewsample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            binding.lvMenu.onItemClickListener = ListItemClickListener()
+        }
+    //リストがタップされたときの処理が記述されたメンバクラス
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            // タップされた定食名を取得
+            val item = parent.getItemAtPosition(position) as String
+            // トーストで表示する文字列を生成
+            val show = "あなたが選んだ定食： $item"
+            // トーストの表示
+            Toast.makeText(this@MainActivity, show, Toast.LENGTH_LONG).show()
+        }
+    }
+}
