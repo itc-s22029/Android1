db1ff37aec49af67c44e949402a29dcbe6173db8
itc-s22029 <s22029@std.it-college.ac.jp>
2023-07-20 17:45:18 +0900

    chap17(pre) commit

---
+++ ListViewReplaceSample/app/src/main/java/jp/ac/it_college/std/s22029/listviewreplacesample/MenuListAdapter.kt
@@ -0,0 +1,32 @@
+package jp.ac.it_college.std.s22029.listviewreplacesample;
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import jp.ac.it_college.std.s22029.listviewreplacesample.databinding.MenuRowBinding
+
+class MenuListAdapter (
+    private val data: List<String>
+    ) : RecyclerView.Adapter<MenuListAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding: MenuRowBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = MenuRowBinding.inflate(inflater)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.tvMenu.text = data[position]
+        holder.binding.root.setOnClickListener {
+            val msg = it.context.resources.getText(R.string.toast_msg, data[position])
+            Toast.makeText(it.context, msg, Toast.LENGTH_LONG).show()
+    }
+    }
+
+    }
+
